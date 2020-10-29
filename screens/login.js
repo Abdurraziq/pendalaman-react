0dc6c2cace417e10d337620d38ece91e7d15c0b7
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-10-29 16:08:18 +0800

    Add Logo and Form.

--- screens/login.js
+++ screens/login.js
@@ -1,13 +1,82 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import {
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize,
+} from "react-native-responsive-dimensions";
+import logo from "../assets/logo.png";
 
 const Login = ({ login }) => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>You haven't logged in yet.</Text>
-      <Button title="Login" onPress={login} />
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.title}>Silahkan Login 😀</Text>
+      <TextInput
+        style={[styles.wrapper, styles.textInput]}
+        placeholder="Email"
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={[styles.wrapper, styles.textInput]}
+        placeholder="Password"
+        secureTextEntry={true}
+      />
+      <TouchableOpacity onPress={login}>
+        <View style={[styles.wrapper, styles.button]}>
+          <Text style={styles.textButton}>LOGIN</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: 220,
+    height: 100,
+    marginBottom: responsiveHeight(2) 
+  },
+  title: {
+    color: "#2196F3",
+    fontSize: responsiveFontSize(2.5),
+    fontWeight: "bold",
+  },
+  wrapper: {
+    width: responsiveWidth(90),
+    height: responsiveHeight(7),
+    borderRadius: 10,
+    marginTop: responsiveHeight(3),
+  },
+  textInput: {
+    borderColor: "#777",
+    borderWidth: 1,
+    paddingHorizontal: responsiveWidth(3),
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#43b2ec",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  textButton: {
+    fontWeight: "bold",
+    color: "#fff",
+    fontSize: responsiveFontSize(2.2),
+  },
+});
 
 export default Login;
